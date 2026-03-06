77fd065cfaf33c0f04a533e2d505d3e12c88e134
AkshatNetworkHub <its.akshatnetworkhub23@gmail.com>
2026-03-06 10:17:36 +0530

    Create linkedIn_db.js

---
+++ linkedIn_db.js
@@ -0,0 +1,113 @@
+// ============================================
+        // LINKEDIN POSTS DATABASE
+        // ============================================
+        const LinkedIn_Post_dB = [
+            {
+                id: 1,
+                title: "Elevating Frontend Engineering: A Roadmap from Syntax to Systems",
+                description: "The journey of a web developer is often viewed as a linear path from HTML to JavaScript. However, true mastery lies in understanding how structure, aesthetics, and logic intersect to create seamless user experiences.",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7424814478535385088/",
+                date_of_publication: "2026-02-09",
+                author: "Akshat Prasad",
+                likes: 7,
+                comments: 2,
+                shares: 0,
+                category: "Skill Development"
+            },
+            {
+                id: 2,
+                title: "Data Analytics Completion Certificate",
+                description: "I am excited to share that I have successfully completed the 30 Days MasterClass in Data Analytics conducted by NoviTech R&D Private Limited (January 19th – February 24th, 2026).",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7432437736344821760/",
+                date_of_publication: "2026-02-24",
+                author: "Akshat Prasad",
+                likes: 1,
+                comments: 0,
+                shares: 0,
+                category: "Sharing Achievements"
+            },
+            {
+                id: 3,
+                title: "🎓 Proud to Share My BCA Semester-1 Achievement!",
+                description: "I am pleased to announce that I have successfully completed my BCA Semester-1 from Maharshi Dayanand University, Rohtak with 442/550 marks, securing 80.36% and an SGPA of 8.82.",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7432068263297236992/",
+                date_of_publication: "2026-02-25",
+                author: "Akshat Prasad",
+                likes: 2,
+                comments: 0,
+                shares: 0,
+                category: "Sharing Achievements"
+            },
+            {
+                id: 4,
+                title: "Akshat Network Hub - LinkedIn Profile ",
+                description: "Get latest update of new ANH Project and get access to Project Outline (PPT , Pdf) along with Source Code.",
+                link: "https://www.linkedin.com/in/akshat-prasad-anh-b867323b0/",
+                date_of_publication: "2026-02-20",
+                author: "Akshat Prasad ANH",
+                likes: 18,
+                comments: 6,
+                shares: 0,
+                category: "ANH Updates"
+            },
+            {
+                id: 5,
+                title: "🚀 Type-Aware Calculator v3.0 — Smarter Division, Real Precision 💡",
+                description: "My Latest Java Project upgrade fixes a common yet overlooked issue. This is a type-aware calculator which can perform operation on differnt type ( int, float, double ) of input and generate output wrt type hierarchy: int < float < double.",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7393997541152829440/",
+                date_of_publication: "2026-02-15",
+                author: "Akshat Prasad ANH",
+                likes: 3,
+                comments: 0,
+                shares: 0,
+                category: "Java Programming"
+            },
+            {
+                id: 6,
+                title: "🚀Maching Learning in the AWS Cloud💡",
+                description: "I found this Post helpful and save it hosted by Jiya Yadav. Add Intelligence to Applications with Amazon SageMaker and Amazon Recognition.",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7393997541152829440/",
+                date_of_publication: "2026-01-03",
+                author: "Jiya Yadav",
+                likes: 132,
+                comments: 3,
+                shares: 26,
+                category: "Machine Learning"
+            },
+            {
+                id: 7,
+                title: "🚀 Project Update | Learning & Development",
+                description: "Today I worked on improving my web development skills by building a personalized dashboard system and exploring better UI architecture.",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7435518182091587585/?originTrackingId=UniSv4YDR%2B%2FM7Dpqk6V5Ew%3D%3D",
+                date_of_publication: "2026-03-06",
+                author: "Akshat Prasad ANH",
+                likes: 4,
+                comments: 2,
+                shares: 0,
+                category: "Update Announcement"
+            },
+            {
+                id: 8,
+                title: "💡 Uploading Computer Graphic Practical File",
+                description: "🚀 Building the Akshat Network Hub Ecosystem. As part of my continuous journey in Web Development, System Architecture, and Knowledge Sharing, I have been actively building a structured digital ecosystem called Akshat Network Hub.",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7435384591982571520/",
+                date_of_publication: "2026-03-05",
+                author: "Akshat Prasad",
+                likes: 4,
+                comments: 2,
+                shares: 0,
+                category: "Resource Announcement"
+            },
+            {
+                id: 9,
+                title: "🚀 Project Update | Learning & Development",
+                description: "Today I worked on improving my web development skills by building a personalized dashboard system and exploring better UI architecture.",
+                link: "https://www.linkedin.com/feed/update/urn:li:activity:7435386228671205377/?originTrackingId=YcR7Td6NW5gRp8CzJcjR0g%3D%3D",
+                date_of_publication: "2026-03-05",
+                author: "Akshat Prasad",
+                likes: 4,
+                comments: 2,
+                shares: 0,
+                category: "Update Announcement"
+            }
+        ];
